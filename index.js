96c5762f811bf5e00f16d2dbbf1ddaad29677297
martinpercu <97993590+martinpercu@users.noreply.github.com>
2024-07-30 13:28:50 -0400

    starting commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+
+const cors = require("cors");
+const stripe = require("stripe")("sk_test_51PMADwRtorj52eamj42PVhENi4pZTMEOlOuP68cHhlxC4dZiqzfE955gCc2UB2aoZpdjolU9j6H1Gy5HvZgjMpdh00lx4pDAfC");
+
+const app = express();
+const port = 3000;
+
+
+
+app.use(express.json());
+
+
+const whitelist = ['http://localhost:4200', 'https://api.stripe.com', 'https://vineyardsinandes.web.app', 'https://tupungatowineco.com'];
+const options = {
+  origin: (origin, callBack) => {
+    if (whitelist.includes(origin)) {
+      callBack(null, true)
+    } else {
+      callBack(new Error('no permission'))
+    }
+  }
+}
+
+app.use(cors(options));
+
+
+app.post('/create_user', async (req, res) => {
+
+  const user = req.body.user;
+  console.log('aca etamos');
+  console.log(user);
+  const customer = await stripe.customers.create(user);
+  console.log(customer);
+  res.send(customer)
+
+});
+
+
+
+
+
+app.listen(3000, () => {
+  console.log('We are in port ==>  ' + port);
+});
